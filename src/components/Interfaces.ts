e3bdb1283d12f4c760b7520ad56149d2af3afb29
PaAdRaAg <pramos12@ucol.mx>
2024-03-22 00:31:12 -0600

    generación de archivo para interfaces de vista de administrador

---
+++ src/components/Interfaces.ts
@@ -0,0 +1,8 @@
+export interface UserList {
+    id_invitation: string;
+    url_invitation: string;
+    date_invitation: string;
+    name_invitation: string;
+    status_invitation: string;
+    name_user: string;
+}
