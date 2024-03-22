a1534fd969ba33befebaf8a04b4d9f8afb93dc54
PaAdRaAg <pramos12@ucol.mx>
2024-03-22 00:33:32 -0600

    vista de invitaciones, validación según el tipo de usuario que haya iniciado sesión se muestra o el componente para administrador o el de ususario

---
+++ src/components/InvitacionesView.tsx
@@ -0,0 +1,34 @@
+import { Row } from "antd";
+import UserAdminView from "./tipos-usuarios/user-admin/UserAdminView";
+import UserUserView from "./tipos-usuarios/UserUserView";
+import { useLanguageInvitations } from "../utils/translations/i18n";
+
+const InvitacionesView: React.FC = () => {
+    const { t } = useLanguageInvitations();
+
+    // ADMIN
+    const userDat = localStorage.getItem('userData');
+    // USER
+    // const userDat = localStorage.getItem('userData2');
+    const userData = userDat ? JSON.parse(userDat) : null;
+
+    return (
+        <div
+            style={{
+                height: '100%',
+                background: '#fff',
+                borderRadius: 8,
+            }}
+        >
+            <Row style={{ margin: '0 0 0 30px', height: '60px' }}>
+                <h1 style={{ margin: '10px 20px' }}>{t('I1')}</h1>
+            </Row>
+
+            <div style={{ margin: '0 0 0 60px', height: '100%' }}>
+                {userData && userData.id_type === '1' ? <UserAdminView /> : <UserUserView />}
+            </div>
+        </div>
+    );
+}
+
+export default InvitacionesView;
