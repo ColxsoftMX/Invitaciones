8b09ded05757e22dbf5ae15bbf44aa40f40d7f11
PaAdRaAg <pramos12@ucol.mx>
2024-03-25 00:11:48 -0600

    archivo con datos para pruebas de lista de inviaciones

---
+++ src/components/tipos-usuarios/user-user/datPrueb.ts
@@ -0,0 +1,263 @@
+export const data =
+    [
+        {
+            "aceptado": true,
+            "token": "92606a6413b88d6f983dcb7479b47a52",
+            "data": {
+                "familia": "Ceballos Andrade",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "e8605b2d01efcd58733a8c2d9450121c",
+            "data": {
+                "familia": "VIRI Y FAM.",
+                "boletos": 3
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "4ec0c5b2e79eaa9162ba74798108aeaa",
+            "data": {
+                "familia": "Salvador",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "b54004892b2835534ae61f99705e9a31",
+            "data": {
+                "familia": "PRIMO JOEL",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "1d9826a6f200c950a19ef37d7e4a9182",
+            "data": {
+                "familia": "TIA NORMA",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "1a32a2a5c794f93c416f468e76f6e7bc",
+            "data": {
+                "familia": "MOY HERNANDES ",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "8ac2653745a52869ac4d965f557ebf7c",
+            "data": {
+                "familia": "Martinez Langarica",
+                "boletos": 3
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "fd0ea6d8f6940791bb873c04119820c2",
+            "data": {
+                "familia": "Castañeda de la Mora",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "9b53bdec19420c617ce453e0843db48f",
+            "data": {
+                "familia": "Kevin ",
+                "boletos": 1
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "5162a9d9d2ac65cdbb43ecb43456d00e",
+            "data": {
+                "familia": "Prima chole",
+                "boletos": 1
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "f5fcf13e38d7836fc2df33913d442f52",
+            "data": {
+                "familia": "Lupita",
+                "boletos": 1
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "f089e799305d8996c5072f90e99836d8",
+            "data": {
+                "familia": "RODRIGO",
+                "boletos": 1
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "efc9299ac075bd2fd060875d97cd9f94",
+            "data": {
+                "familia": "Trini y Chuy",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "e21a8c0c1476e00986c1fd0fd34580e5",
+            "data": {
+                "familia": "Anahi ",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "b328ecadf5ab406053ee98eacc86a38f",
+            "data": {
+                "familia": "Noel",
+                "boletos": 1
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "7e945e3e91a4c054d534b41a5b0b0a38",
+            "data": {
+                "familia": "Anahi y gustavo ",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "bb2bca298a34615e12b394daa2e03f1c",
+            "data": {
+                "familia": "Fam. Castañeda aldaco",
+                "boletos": 3
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "ca788361e2d6c7bc8bc8dfe68fd37ae6",
+            "data": {
+                "familia": "Fam. Magaña Munguia ",
+                "boletos": 4
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "c7ffa6ca463b988948800568ac87f2b5",
+            "data": {
+                "familia": "Angela Sevilla",
+                "boletos": 1
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "9d6eafb72fd057f58b45a46ac5048de5",
+            "data": {
+                "familia": "Carrillo Castañeda",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "ff8a95cdffdff742cf8e6ed513479be9",
+            "data": {
+                "familia": "Cortés Carrillo",
+                "boletos": 3
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "7245b38464d2b02a09fcc65ba4d76437",
+            "data": {
+                "familia": "Preciado Carrillo",
+                "boletos": 5
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "65221d0673d4d17c640cffdda0df36c0",
+            "data": {
+                "familia": "Carrillo Cortés",
+                "boletos": 4
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "956a612beaa35766749acf3f4f9b0614",
+            "data": {
+                "familia": "Familia Roja",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "ddb3fb9116da09a7dacea48b2ef3e786",
+            "data": {
+                "familia": "Familia falsa que voy a borrar al rato",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "73e53ff16c5da7183231ce6b9a31ffd0",
+            "data": {
+                "familia": "Jaja",
+                "boletos": 20
+            }
+        },
+        {
+            "aceptado": false,
+            "token": "f372cf6c451dbc3c6554023dac8069b4",
+            "data": {
+                "familia": "salvador",
+                "boletos": 60
+            }
+        },
+        {
+            "aceptado": true,
+            "token": "3cb929b6fd9ad7961df9a60e1f410dd3",
+            "data": {
+                "familia": "dsf",
+                "boletos": 4
+            }
+        }
+    ]
+
+export const data2 =
+    [
+        {
+            "aceptado": 1,
+            "token": "92606a6413b88d6f983dcb7479b47a52",
+            "data": {
+                "familia": "Ceballos Andrade",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": 2,
+            "token": "e8605b2d01efcd58733a8c2d9450121c",
+            "data": {
+                "familia": "VIRI Y FAM.",
+                "boletos": 3
+            }
+        },
+        {
+            "aceptado": 3,
+            "token": "4ec0c5b2e79eaa9162ba74798108aeaa",
+            "data": {
+                "familia": "Salvador",
+                "boletos": 2
+            }
+        },
+        {
+            "aceptado": 4,
+            "token": "30370c99397a10211123a517e4d89920",
+            "data": {
+                "familia": "Fierro Martinez",
+                "boletos": 3
+            }
+        },
+    ]
